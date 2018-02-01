ff805b3c03af7cc5a05751026e8b9057d8af0b7a
hinrek saar <hinrek@gmail.com>
2018-02-01 19:23:39 +0200

    Timer for main() and read file content with Stream

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+import java.nio.file.Files;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+	    String fileName = "timing.log";
+
+	    try(Stream<String> stream = Files.lines(Paths.get(fileName))) {
+
+            stream.forEach(System.out::println);
+
+        } catch (IOException e){
+	        e.printStackTrace();
+        }
+
+        long endTime   = System.nanoTime();
+        long seconds = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
+        System.out.println("Duration: " + seconds + "ms");
+    }
+}
