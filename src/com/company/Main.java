608f4c260e0766a45427f71fbc80d90fda37c925
hinrek saar <hinrek@gmail.com>
2018-02-08 21:43:45 +0200

    Changed file reading to Stream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,41 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
         String fileName = "timing.log";
-
-        Pattern pattern = Pattern.compile(" ");
-        try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
-            List<Log> logs = in
-                    .lines()
-                    .map(line -> {
-                        String[] arr = pattern.split(line);
-                        return new Log(Integer.parseInt(arr[arr.length - 1]),
-                                arr[4]);
-                    })
+        
+        try (Stream<String> in = Files.lines(Paths.get(fileName))) {
+            Pattern pattern = Pattern.compile(" ");
+            List<Log> logs;
+            logs = in
+                    .map(pattern::split)
+                    .map((arr) -> new Log(Integer.parseInt(arr[arr.length - 1]),
+                            arr[4]))
                     .collect(Collectors.toList());
+            in.close();
 
-            Map<String, Double> logsByRequestResourceAndAverageDuration = logs
+            Map<String, Double> logsGrouped;
+            logsGrouped = logs
                     .stream()
                     .collect(Collectors.groupingBy(Log::getRequestResourceName,
                             Collectors.averagingInt(Log::getRequestDuration)));
-
-            logsByRequestResourceAndAverageDuration
+            logsGrouped
                     .entrySet()
                     .stream()
                     .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                    .limit(1000)
+                    .limit(10)
                     .forEach(entry -> System.out.printf("ms: %8.1f | Resource name: %s%n", entry.getValue(), entry.getKey()));
-
         } catch (IOException e) {
             e.printStackTrace();
         }
