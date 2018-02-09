4dd408c15442d360ef601a7f072a2702e314d0ad
hinrek saar <hinrek@gmail.com>
2018-02-09 13:27:43 +0200

    Made some functions from spaghetti

--- src/com/company/Log.java
+++ src/com/company/Log.java
@@ -1,5 +1,15 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Log {
     private String requestResourceName;
     private int requestDuration;
@@ -12,17 +22,56 @@ public class Log {
         this.requestResourceName = requestResourceName;
     }
 
-    public String getRequestResourceName() {
+    public void printLogs(Map<String, Double> logs, int logLimit) {
+        if (logs != null) {
+            logs
+                    .entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(logLimit)
+                    .forEach(entry -> System.out.printf("ms: %8.1f | Resource name: %s%n", entry.getValue(), entry.getKey()));
+        }
+    }
+
+    public Map groupLogsByName(List<Log> logs) {
+        Map<String, Double> logsGrouped;
+        if (logs != null) {
+            logsGrouped = logs
+                    .stream()
+                    .collect(Collectors.groupingBy(Log::getRequestResourceName,
+                            Collectors.averagingInt(Log::getRequestDuration)));
+            return logsGrouped;
+        }
+        return null;
+    }
+
+    public List<Log> readFile(String fileName) throws NullPointerException {
+        List<Log> logs = null;
+        try (Stream<String> in = Files.lines(Paths.get(fileName))) {
+            Pattern pattern = Pattern.compile(" ");
+            logs = in
+                    .map(pattern::split)
+                    .map((arr) -> new Log(Integer.parseInt(arr[arr.length - 1]),
+                            arr[4]))
+                    .collect(Collectors.toList());
+            in.close();
+        } catch (IOException e) {
+            System.out.println("File not found!");
+        }
+        return logs;
+    }
+
+    private String getRequestResourceName() {
         return replaceCharactersInString(requestResourceName);
     }
 
-    private String replaceCharactersInString(String string) {
+    private String replaceCharactersInString(String initialString) {
         // https://regexr.com/3ke4j
         String pattern = "(=\\d+)|(=CUS\\w+)";
-        return string.replaceAll(pattern, "");
+        return initialString.replaceAll(pattern, "");
     }
 
-    public int getRequestDuration() {
+    private int getRequestDuration() {
         return requestDuration;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,44 @@
 package com.company;
 
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.nio.file.Files;
-import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        String fileName = "timing.log";
-        
-        try (Stream<String> in = Files.lines(Paths.get(fileName))) {
-            Pattern pattern = Pattern.compile(" ");
-            List<Log> logs;
-            logs = in
-                    .map(pattern::split)
-                    .map((arr) -> new Log(Integer.parseInt(arr[arr.length - 1]),
-                            arr[4]))
-                    .collect(Collectors.toList());
-            in.close();
 
-            Map<String, Double> logsGrouped;
-            logsGrouped = logs
-                    .stream()
-                    .collect(Collectors.groupingBy(Log::getRequestResourceName,
-                            Collectors.averagingInt(Log::getRequestDuration)));
-            logsGrouped
-                    .entrySet()
-                    .stream()
-                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                    .limit(10)
-                    .forEach(entry -> System.out.printf("ms: %8.1f | Resource name: %s%n", entry.getValue(), entry.getKey()));
-        } catch (IOException e) {
-            e.printStackTrace();
+        String fileName;
+        int logLimit;
+
+        Log logs = new Log();
+
+        if (args.length < 1) {
+            System.out.println(printHelp());
+        } else if (args[0].equals("-h") || args[0].equals("-help")) {
+            System.out.println(printHelp());
+        } else if (args.length == 2) {
+            try {
+                fileName = args[0];
+                logLimit = Integer.parseInt(args[1]);
+
+                Map groupedLogs = logs.groupLogsByName(logs.readFile(fileName));
+                logs.printLogs(groupedLogs, logLimit);
+            } catch (NumberFormatException e) {
+                System.err.println("Argument " + args[1] + " must be an integer.");
+                System.exit(1);
+            }
+        } else {
+            System.out.println(printHelp());
         }
 
         long endTime = System.nanoTime();
         long seconds = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
-        System.out.println("Duration: " + seconds + "ms");
+        System.out.println("\nDuration: " + seconds + "ms");
+    }
+
+    private static String printHelp() {
+        return "Usage: scaling-engine.jar [Filename] [Rows] " +
+                "\n e.g java -jar scaling-engine.jar timing.log 10";
     }
 }
